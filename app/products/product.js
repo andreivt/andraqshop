9349d2ec49c353e64e6a9ffadb46a5cf1aefd383
nikodemus0715 <andrei.tertiscu@gmail.com>
2016-07-08 12:51:14 +0300

    js altele

--- app/products/product.js
+++ app/products/product.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var productID = getParameterByName('id');
     var products = getProductList();
     var currentProduct = products[productID];
-
+    if(localStorage['cart'] === undefined){
+      localStorage['cart'] = JSON.stringify([]);
+    }
     var title = document.querySelector('.product-summary>h2');
     title.innerHTML = currentProduct.name;
 
@@ -58,4 +60,37 @@ document.addEventListener('DOMContentLoaded', function() {
       <td>` + currentProduct.reviews[k].text + `</td>
   </tr>`;
     }
+
+    var addToCartBtn = document.querySelector('#add-to-cart-btn');
+    addToCartBtn.addEventListener('click', function(){
+      var curentCart = JSON.parse(window.localStorage['cart']);
+      for(var i=0;i<curentCart.length;i++){
+        if(curentCart[i].id == productID){
+          curentCart[i].qty = parseInt(curentCart[i].qty);
+          curentCart[i].qty += parseInt(document.querySelector('.qty').value);
+          localStorage.cart = JSON.stringify(curentCart);
+          return;
+        }
+      }
+      var newProduct = {  id : productID,
+                          qty: document.querySelector('.qty').value,
+                          name: document.querySelector('.product-summary>h2').innerHTML,
+                          price: price.innerHTML,
+                          picture: currentProduct.picture
+                        };
+
+      curentCart.push(newProduct);
+      localStorage.cart = JSON.stringify(curentCart);
+    })
+
+    var minus = document.querySelector('.minus');
+    var plus = document.querySelector('.plus');
+    var qty = document.querySelector('.qty');
+    minus.addEventListener('click', function(){
+      if(qty.value > 0)
+        qty.value--;
+    })
+    plus.addEventListener('click', function(){
+      qty.value++;
+    })
 })
